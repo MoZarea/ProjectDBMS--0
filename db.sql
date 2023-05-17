cb03a5b230174e8e996dbfd02df3af784f7f5b2c
Abdullah <abdallah0115019@gmail.com>
2023-05-17 20:48:53 +0300

    create auth table

--- db.sql
+++ db.sql
@@ -1,2 +1,9 @@
 create database project;
 use project;
+CREATE TABLE auth (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  pass VARCHAR(255) NOT NULL
+);
+INSERT INTO auth (username, pass) VALUES ('abdullah', '123');
+SELECT * FROM auth;
